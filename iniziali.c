a7b426ac77dd98890775fb271c4f9a953762568b
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-23 15:58:14 +0100

    Modifica

--- iniziali.c
+++ iniziali.c
@@ -2,33 +2,26 @@
 
 int main(){
 
-printf("\n \n");
+ printf("\n \n");
 
-printf("LLLLLLLLLLLLLLLLLLLL\n");
-printf("L\n");
-printf("L\n");
-printf("L\n");
-printf("L\n");
-printf("L\n");
+ printf("LLLLLLLLLLLLLLLLLLLL\n");
+ printf("L\n");
+ printf("L\n");
+ printf("L\n");
+ printf("L\n");
+ printf("L\n");
 
-printf("\n \n");
+ printf("\n \n");
 
-printf("             V\n");
-printf("        V\n");
-printf("   V\n");
-printf("V\n");
-printf("   V\n");
-printf("        V\n");
-printf("            V\n");
-
-
+ printf("             V\n");
+ printf("        V\n");
+ printf("   V\n");
+ printf("V\n");
+ printf("   V\n");
+ printf("        V\n");
+ printf("            V\n");
 
 
 
-
-
-
-
-
-
+return 0return 0;;
 }
