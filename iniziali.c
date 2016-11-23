74b23001db280f39e365ea0d7ff0abf5d21b0655
alunno <alunno@debian13>
2016-11-23 13:58:44 +0100

    Iniziali

---
+++ iniziali.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+printf("\n \n");
+
+printf("LLLLLLLLLLLLLLLLLLLL\n");
+printf("L\n");
+printf("L\n");
+printf("L\n");
+printf("L\n");
+printf("L\n");
+
+printf("\n \n");
+
+printf("             V\n");
+printf("        V\n");
+printf("   V\n");
+printf("V\n");
+printf("   V\n");
+printf("        V\n");
+printf("            V\n");
+
+
+
+
+
+
+
+
+
+
+
+}
