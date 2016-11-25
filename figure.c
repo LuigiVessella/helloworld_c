17d188f7d239cadac37cbe5b94ead5c25f65d66c
LuigiVessella <vessellaluigi12@gmail.com>
2016-11-25 16:11:10 +0100

    figure

---
+++ figure.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+int main(){
+
+ printf("\n\n");
+
+ printf("                *    \n");
+ printf("               ***    \n");
+ printf(" **********   *****   \n");
+ printf(" *        *     *    \n");
+ printf(" *        *     *    \n");
+ printf(" *        *     *    \n");
+ printf(" *        *     *    \n");
+ printf(" *        *     *     \n");
+ printf(" *        *     *    \n");
+ printf(" *        *     *    \n");
+ printf(" **********     *    \n");
+
+ printf("\n\n\n");
+
+ printf("                     *               \n");
+ printf("    ***             * *               \n");
+ printf("   *   *           *   *              \n");
+ printf("  *     *         *     *               \n");
+ printf("  *     *        *       *           \n");
+ printf("  *     *       *         *        \n");
+ printf("  *     *        *       *       \n");
+ printf("  *     *         *     *       \n");
+ printf("  *     *          *   *        \n");
+ printf("   *   *            * *         \n");
+ printf("    ***              *          \n");
+ 
+ printf("\n\n");
+
+ printf("Eccovi finalmente le figure prof! \n");
+
+
+
+ return 0;
+
+}
+
