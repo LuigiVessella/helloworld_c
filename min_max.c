3470436e4431ecf4fbdba8b5a49c643673db0e25
Luigi Vessella <vessellaluigi12@gmail.com>
2016-11-27 14:59:37 +0100

    min_max

---
+++ min_max.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<limits.h>
+
+//#  define INT_MIN	(-INT_MAX - 1)
+//#  define INT_MAX	2147483647
+
+int main(){
+
+   printf("INT_MIN e': %d ", INT_MIN);
+   printf("\n");
+   printf("INT_MAX e': %d", INT_MAX);
+   printf("\n");
+  
+   
+ 
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
