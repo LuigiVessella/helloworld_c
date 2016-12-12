146b83779b0813c9aac142fd5c9dee5bb740365b
alunno <alunno@debian16>
2016-12-12 09:18:42 +0100

    File

--- maggiore_tra_tre.c
+++ maggiore_tra_tre.c
@@ -2,21 +2,54 @@
 
 int main(){
 int a,b,c;
+int max = 0,min=0;
 printf("Inserisci tre interi: ");
-scanf("%d %d %d",&a, &b, &c);
+scanf(" %d %d %d",&a, &b, &c);
 printf("La somma e' %d\n", a+b+c );
-printf("Il prodotto e' %d\n",a*b+c);
+printf("Il prodotto e' %d\n",a*b*c);
+printf("La media e' %d\n",(a+b+c)/3);
 
-if(a > b && b > c){
-   printf("Il maggiore e' %d\n",a);
-}
-if(b > a && b > c)
+max=a;
+if (a==b && b==c)
 {
-    printf("Il maggiore e' %d\n",b);
+    printf("I tre numeri sono uguali\n");
 }
-if(c>a && c>b)
+if (b>max)
 {
-    printf("Il maggiore e' %d\n",c);
+    max=b;
 }
+if (c>max)
+{
+    max=c;
+}
+
+min = a;
+
+if (b<min)
+{
+    min=b;
+}
+if (c<b)
+{
+    min=c;
+}
+printf("%d e' il maggiore\n", max);
+printf("%d e' il minore\n", min);
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
