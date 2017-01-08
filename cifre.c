9b38773e1d430d607b62f2520e31c0970f892f26
LuigiVessella <vessellaluigi12@gmail.com>
2017-01-08 15:53:39 +0100

    modifiche

--- cifre.c
+++ cifre.c
@@ -3,24 +3,29 @@
 int main(){
 
 int num,modulo;
-
+int a,b,c,d,e;
 printf("Inserisci numero di 5 cifre: ");
 scanf("%d",&num);
 
 modulo = num%10;
-printf("%d   ", modulo);
+a = modulo;
+
 num = num/10;
 modulo = num%10;
-printf("%d   ", modulo);
+b=modulo;
+
 num = num/10;
 modulo = num%10;
-printf("%d   ", modulo);
+c=modulo;
+
 num = num/10;
 modulo = num%10;
-printf("%d   ", modulo);
+d=modulo;
+
 num=num/10;
 modulo = num%10;
-printf("%d   ", modulo);
+e=modulo;
+printf("%d %d %d %d %d   ", e,d,c,b,a);
 
 return 0;
 }
